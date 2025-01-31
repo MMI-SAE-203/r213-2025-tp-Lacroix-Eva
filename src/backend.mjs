24e63f2365a6d8babc5fa10bf30d9eeb7ca61023
Lacroix-Eva <eva.lacroix@edu.univ-fcomte.fr>
2025-01-31 14:12:18 +0100

    1er commit demandé

---
+++ src/backend.mjs
@@ -0,0 +1,19 @@
+import PocketBase from 'pocketbase';
+
+const pb = new PocketBase('http://127.0.0.1:8090/');
+
+export async function getOffres() {
+    try {
+        let data = await pb.collection('Agence').getFullList({
+            sort: '-created',
+        });
+        data = data.map((a) => {
+            a.img= pb.files.getURL(a, a.imgUrl);
+            return a;
+        });
+        return data;
+    } catch (error) {
+        console.log('Une erreur est survenue en lisant la liste des maisons', error);
+        return [];
+    }
+}
