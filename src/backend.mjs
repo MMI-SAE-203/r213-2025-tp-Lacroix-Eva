e5bb630b2af1ca064a1489a3362e504fd568c80a
Lacroix-Eva <eva.lacroix@edu.univ-fcomte.fr>
2025-02-14 14:42:20 +0100

    filtrage offres par prix

--- src/backend.mjs
+++ src/backend.mjs
@@ -64,3 +64,20 @@ export async function addOffre(house) {
         };
     }
 }
+
+export async function filterByPrix(prixMin, prixMax) {
+    try {
+        let data = await pb.collection('Agence').getFullList({
+            sort: '-created',
+            filter: `prix >= ${prixMin} && prix <= ${prixMax}`
+        });
+        data = data.map((maison) => {
+            maison.img = pb.files.getURL(maison, maison.images);
+            return maison;
+        });
+        return data;
+    } catch (error) {
+        console.log('Une erreur est survenue en filtrant la liste des maisons', error);
+        return [];
+    }
+}
